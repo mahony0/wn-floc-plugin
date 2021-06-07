ce31274651986f95b08f4ffce5f56c4adbcae119
Mahmut Namli <mahmudnamli@gmail.com>
2021-06-07 10:59:12 +0300

    DISABLE_FLOC true/false fix

--- Plugin.php
+++ Plugin.php
@@ -11,16 +11,16 @@ class Plugin extends PluginBase
     public function pluginDetails()
     {
         return [
-            "name" => "Disable FLoC",
-            "description" => "Plugin for disabling FLoC on WinterCMS",
-            "author" => "Mahony0",
-            "icon" => "icon-google"
+            'name' => 'Disable FLoC',
+            'description' => 'Plugin for disabling FLoC on WinterCMS',
+            'author' => 'Mahony0',
+            'icon' => 'icon-google'
         ];
     }
 
     public function boot()
     {
-        if ($isActive = env('DISABLE_FLOC', false)) {
+        if (! $isDisabled = env('DISABLE_FLOC', false)) {
             $kernel = $this->app->make(Kernel::class);
             $kernel->pushMiddleware(NoFLoCMiddleware::class);
         }
